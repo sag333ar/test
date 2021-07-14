6a87d20d98b58fb1ee71636e338efb8390ab6868
Sagar Kothari <sagar.kothari@datarobot.com>
2021-07-14 08:17:39 +0530

    added test code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch("https://api.github.com/search/users?q=sagar")
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setData(response.items)
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +34,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <h3>Users</h3>
+        {data && data.map((user) => (
+          <div key={user.login}>
+            <img src={user.avatar_url} alt={user.login} />
+            <p>{user.login}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
